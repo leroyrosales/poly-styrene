5c7f75f47eecb6e692cbe1200430c5fe8e20cd82
Leroy Rosales <leroyrosales@gmail.com>
2022-02-14 20:34:30 -0600

    Content and layout chagnes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,15 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				'black': '#020202',
+				'dark-green': '#0D2818',
+				'mid-green': '#04471C',
+				'green': '#058C42',
+				'light-green': '#16DB65',
+				'white': '#ECF0F1'
+			}
+		},
+	}
 };
