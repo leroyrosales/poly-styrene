2df7cee6a75e637bebbf72535ce083ead7f2ac42
Leroy Rosales <leroy.rosales@austin.utexas.edu>
2026-04-09 22:38:03 -0500

    Updates seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,8 +9,6 @@ import preact from '@astrojs/preact';
 
 // @ts-check
 export default /** @type {import('astro').AstroUserConfig} */ ({
-	// Enable the Preact renderer to support Preact JSX components.
-	integrations: [
-		preact(),
-	  ],
+	site: 'https://leroyrosales.com',
+	integrations: [preact()],
 });
